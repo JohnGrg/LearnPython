11614257627af17a3123e5627307cc35a50baaf5
John George <j.john.developer@gmail.com>
2019-08-03 18:45:48 +0530

    Update TestingPython1.py

--- TestingPython1.py
+++ TestingPython1.py
@@ -1,4 +1,4 @@
-cars = 100
+cars = 1000000
 space_in_a_car = 4.0
 drivers = 30
 passengers = 90
@@ -8,7 +8,7 @@ carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
 
 
-print("There are", cars, "cars available.")
+print("There are", cars, "cars available hello.")
 print("There are only", drivers, "drivers available.")
 print("There will be", cars_not_driven, "empty cars today.")
 print("We can transport", carpool_capacity, "people today.")
